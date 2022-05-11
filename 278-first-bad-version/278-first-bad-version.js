0e260bc811d0708c630743c177666bc25987de77
Calvin Chau <48419130+mangohead33@users.noreply.github.com>
2022-05-11 16:08:11 -0700

    Time: 76 ms (53.17%), Space: 41.4 MB (94.49%) - LeetHub

---
+++ 278-first-bad-version/278-first-bad-version.js
@@ -0,0 +1,48 @@
+/**
+ * Definition for isBadVersion()
+ * 
+ * @param {integer} version number
+ * @return {boolean} whether the version is bad
+ * isBadVersion = function(version) {
+ *     ...
+ * };
+ */
+
+/**
+ * @param {function} isBadVersion()
+ * @return {function}
+ */
+var solution = function(isBadVersion) {
+    /**
+     * @param {integer} n Total versions
+     * @return {integer} The first bad version
+     */
+    
+    return function(n) {
+        let start = 0;
+        let end = n;
+        const findMid = (start, end) => {
+            return Math.floor((start + end)/2);
+        };
+        let mid = findMid(start, end);
+        while (n) {
+            if (isBadVersion(mid)) {
+                if (!isBadVersion(mid - 1)) {
+                    return mid;
+                }
+                end = mid;
+                mid = findMid(start, end);
+    
+            } else {
+                start = mid + 1;
+                mid = findMid(start, end);
+            }
+            // 0, 6 mid = 3
+            // 0 1 2 3 4 5 6
+            // 4,6 mid= 5
+            // 4,5
+            
+        }
+
+    };
+};
