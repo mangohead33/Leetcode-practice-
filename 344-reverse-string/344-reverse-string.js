541fa77dde4cd27b4de5d35ad46d8f0b2e366740
Calvin Chau <48419130+mangohead33@users.noreply.github.com>
2022-05-17 14:13:39 -0700

    Time: 190 ms (7.59%), Space: 49.3 MB (58.70%) - LeetHub

--- 344-reverse-string/344-reverse-string.js
+++ 344-reverse-string/344-reverse-string.js
@@ -3,9 +3,6 @@
  * @return {void} Do not return anything, modify s in-place instead.
  */
 var reverseString = function(s) {
-    if (s.length === 1) {
-        return s;
-    }
     let start = 0;
     let end = s.length - 1;
     while (end > start) {
