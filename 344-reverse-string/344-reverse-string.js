72defe3e50989c979bb226f488719fb4f0360c00
Calvin Chau <48419130+mangohead33@users.noreply.github.com>
2022-05-17 14:13:15 -0700

    Time: 182 ms (9.32%), Space: 49.1 MB (85.58%) - LeetHub

--- 344-reverse-string/344-reverse-string.js
+++ 344-reverse-string/344-reverse-string.js
@@ -3,18 +3,17 @@
  * @return {void} Do not return anything, modify s in-place instead.
  */
 var reverseString = function(s) {
-    let odd = s.length % 2;
-    
-    for (let i = 0; i < s.length; i++) {
-        let temp = s[s.length - 1 - i];
-        if (odd && i === Math.floor(s.length/2)) {
-            break;
-        } 
-        s[s.length - 1 - i] = s[i];
-        s[i] = temp;
-        if (!odd && i === (s.length/2 - 1)) {
-            break;
-        }
+    if (s.length === 1) {
+        return s;
     }
-    return s;
+    let start = 0;
+    let end = s.length - 1;
+    while (end > start) {
+        let swap = s[start];
+        s[start] = s[end];
+        s[end] = swap;
+        start++;
+        end--;
+    }
+    
 };
