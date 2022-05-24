8629aaf9e117e100902dd684465a1ffe74ff8829
Calvin Chau <48419130+mangohead33@users.noreply.github.com>
2022-05-23 18:17:56 -0700

    Time: 74 ms (89.59%), Space: 44 MB (78.00%) - LeetHub

--- 733-flood-fill/733-flood-fill.js
+++ 733-flood-fill/733-flood-fill.js
@@ -6,13 +6,10 @@
  * @return {number[][]}
  */
 var floodFill = function(image, sr, sc, newColor) {
-    // inputs a nested array, sr/sc are coordinates, newColor change image[sr][sc] into newColor and all adjacent  tiles and those adjacent tiles into newcolor as long as they match the first value's value.
-    // output none, you're only modifying original array
     let originalColor = image[sr][sc];
     if (originalColor === newColor) {
         return image;
     }
-    // use recursive function to flood fill
     const recur = (rc, cc) => {
         // rc = row coordinate, cc = column coordinate
         image[rc][cc] = newColor;
@@ -36,7 +33,4 @@ var floodFill = function(image, sr, sc, newColor) {
     }
     recur(sr, sc);
     return image;
-    // issues with current algo. If original color = newColor i run into inf loops
-    // i should also keep track of coordinates that were already modified
-    // or logically, if theres no change in colors just return original array
 };
