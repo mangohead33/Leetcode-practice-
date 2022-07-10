367d3e60312c5c6438db189efda3e9b51284855a
Calvin Chau <48419130+mangohead33@users.noreply.github.com>
2022-07-10 11:36:34 -0700

    Time: 101 ms (50.25%), Space: 44.3 MB (60.00%) - LeetHub

--- 82-remove-duplicates-from-sorted-list-ii/82-remove-duplicates-from-sorted-list-ii.js
+++ 82-remove-duplicates-from-sorted-list-ii/82-remove-duplicates-from-sorted-list-ii.js
@@ -10,13 +10,6 @@
  * @return {ListNode}
  */
 var deleteDuplicates = function(head) {
-    // modify linked list and return it
-    // remove all uplicate numbers
-    // list is already sorted
-    
-    // scenario 1 , the head and following nodes are duplicates!
-    // your algorithim will take a peek at the enxt nodes value and compare values with the current node, make the first node we encounter the enxt head node!
-    // also keep a variable that verifies whether you're at the ehad already.
     if(!head) {
         return head;
     }
@@ -27,7 +20,6 @@ var deleteDuplicates = function(head) {
     let prevNode = currentNode;
     
     while(nextNode) {
-        
         let nextNodeVal = nextNode.val;
         if (currentNode.val !== nextNodeVal) {
             pointingToHeadNode = false;
@@ -37,10 +29,8 @@ var deleteDuplicates = function(head) {
             continue;
         }
         while (currentNode.val === nextNodeVal) {
-            // cycle through linked list til we reach a non duplicate
             nextNode = nextNode.next;
             if  (!nextNode) {
-                // if nextNode is null
                 break;
             }
             nextNodeVal = nextNode.val;
@@ -56,7 +46,6 @@ var deleteDuplicates = function(head) {
             break;
         }
         nextNode = nextNode.next;
-        
     }
     return modifiedHead;
 };
