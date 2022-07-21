8ddd55d845fac0bb2ec5aa109423b2debd03bf19
Calvin Chau <48419130+mangohead33@users.noreply.github.com>
2022-07-21 16:03:07 -0700

    Time: 1003 ms (36.65%), Space: 0B (100.00%) - LeetHub

--- 196-delete-duplicate-emails/196-delete-duplicate-emails.sql
+++ 196-delete-duplicate-emails/196-delete-duplicate-emails.sql
@@ -2,3 +2,9 @@
 # Write your MySQL query statement below
 DELETE p1 FROM Person AS p1, Person as p2
 WHERE p1.email = p2.email AND p1.id > p2.id;
+
+# you're modifying the persons table
+# p1 and p2 represent the same table but gives us a way to compare and remove duplicates
+# Algorithim will delete matching emails
+# p1.id > p2.id will keep the lower valued id, and delete higher id dupes
+# p1.id < p2.id will delete lower value ids and keep latter;
