4b31958eab136977df127c5107cf6820b9913fe4
Calvin Chau <48419130+mangohead33@users.noreply.github.com>
2022-07-21 16:16:38 -0700

    Time: 85 ms (81.68%), Space: 43.4 MB (77.64%) - LeetHub

--- 136-single-number/136-single-number.js
+++ 136-single-number/136-single-number.js
@@ -7,4 +7,8 @@ var singleNumber = function(nums) {
     // if it appears twice you can set the return value to duplicate
     // there should be one mapped entry that returns single
     return nums.reduce((a,b) => a^b)
+    // ^ is the xor operator, if encounter the same number again it'll subtract it
+    // that only works when paired with reduce, something to do with bit manipulation
+    
+    //find 1 unique values in a set with only 2 of the same duplicates
 };
